cfcc0acc56d17f2fdcac6c5f167b0587de35eb2c
Galarce Lucas <galarcelucas@gmail.com>
2024-05-07 10:12:22 -0300

    Initial config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { config } from './config';
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject(config.KEY) private appConfig: ConfigType<typeof config>, // private utilsService: UtilsService,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getInfoApi(): Promise<object> {
+    return this.appConfig.project;
   }
 }
